6ca26af5c0e2345990cd2a669bd0e27d661ef3fd
Esteban Blanc <esteban.blanc@lse.epita.fr>
2021-02-18 16:20:32 +0100

    lw: Working implementation

--- src/controller.sv
+++ src/controller.sv
@@ -4,7 +4,8 @@ module controller(
     output logic [2:0] alu_control,
     output logic reg_write_control,
     output logic sign_ext_control,
-    output logic data_mem_write_control
+    output logic data_mem_write_control,
+    output logic reg_write_data_source_control
 );
 
     logic [9:0] opcode_func3;
@@ -15,19 +16,29 @@ module controller(
             // Integer Register-Immediate
             'b0010011???:
                 begin
-                    alu_control = func3;
-                    reg_write_control = 1;
-                    sign_ext_control = 0;
-                    data_mem_write_control = 0;
+                    alu_control = func3; // Forward func3 to ALU
+                    reg_write_control = 1; // Write
+                    sign_ext_control = 0; // I-type
+                    data_mem_write_control = 0; // No write
+                    reg_write_data_source_control = 0; // ALU out as source
                 end
             // Store
             'b0100011???:
                 begin
                     alu_control = 0; // Add
-                    reg_write_control = 0;
+                    reg_write_control = 0; // No write
                     sign_ext_control = 1; // S-type extension
-                    data_mem_write_control = 1;
-
+                    data_mem_write_control = 1; // Write to ram
+                    reg_write_data_source_control = 0; // Unused
+                end
+            // Load
+            'b0000011???:
+                begin
+                    alu_control = 0; // Add
+                    reg_write_control = 1; // Write
+                    sign_ext_control = 0; // I-type extension
+                    data_mem_write_control = 0; // No write to ram
+                    reg_write_data_source_control = 1; // RAM output as source
                 end
             default:
                 begin
@@ -35,6 +46,7 @@ module controller(
                     reg_write_control = 0;
                     data_mem_write_control = 0;
                     sign_ext_control = 0;
+                    reg_write_data_source_control = 0;
                     $display("CONTROLLER: Opcode=0b%0b, Func3=0b%0b; Not handled", opcode, func3);
                 end
         endcase
