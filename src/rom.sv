cc5f38385c50720444ea87a52e091583b77e1622
Esteban Blanc <esteban.blanc@lse.epita.fr>
2021-02-22 18:21:34 +0100

    jal: Add jal x0, 8 in rom

--- src/rom.sv
+++ src/rom.sv
@@ -8,8 +8,12 @@ module rom(input logic clk, input logic [31:0] addr, output logic [31:0] data);
         rom_data[3] = 32'b0000000_00001_00000_010_00000_0100011; // sw 0(x0), x1
         rom_data[4] = 32'b000000000000_00000_010_00010_0000011; // lw x2, x0
         rom_data[5] = 32'b0_000000_00010_00001_000_0100_0_1100011; // beq x1, x2, 8
-        // rom_data[7] is never run because of beq
+        // rom_data[6] is never run because of beq
         rom_data[7] = 32'b000000000000_00000_000_00000_0010011; // addi x0, x0, 0
+        rom_data[8] = 32'b0_0000000100_0_00000000_00000_1101111; // jal x0, 8
+        // rom_data[9] is never run because of beq
+        rom_data[10] = 32'b000000000000_00000_000_00000_0010011; // addi x0, x0, 0
+
     end
 
     logic [31:0] rom_data [31:0];
